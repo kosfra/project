b5a03e04da1246c38dfb18ab9a989e5c9885a29f
Kostya <kosfrancuz@gmail.com>
2017-04-11 21:55:50 +0000

    changes

---
+++ js/preloader.js
@@ -0,0 +1,6 @@
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(1000).fadeOut('slow');
+});
