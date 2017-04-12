a9a5d83689b02040351b530f6235b9ebebf90436
Kostya <kosfrancuz@gmail.com>
2017-04-12 02:03:36 +0000

    changes

---
+++ js/form.js
@@ -0,0 +1,28 @@
+$('btn btn-default').click(function() {
+    location.href = "contacts.html";
+});
+$(function() {
+    $(':submit').click(sendForm);
+});
+
+function sendForm(e) {
+  var form = document.querySelector('form');
+  if (form.checkValidity()) {
+        e.preventDefault();
+        $.ajax({
+                url: "https://formspree.io/kosfrancuz@gmail.com",
+                method: "POST",
+                data: {
+                    name: $('#client_name').val(),
+                    mob: $('#client_phone').val(),
+                    email: $('#client_email').val(),
+                    client_proposals: $('client_proposals').val(),
+
+                },
+                dataType: "json"
+            })
+            .done(function() {
+                $('form').html("Thx");
+            });
+    }
+}
